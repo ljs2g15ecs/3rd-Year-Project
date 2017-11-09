93ed478a3b88114cced478b1d9f9c75147442fca
ljs2g15ecs <ljs2g15@soton.ac.uk>
2017-11-09 23:06:39 +0000

    Round Function
    Added Round Function and Key Schedule basics

--- Development/SIMON/SIMON_host.h
+++ Development/SIMON/SIMON_host.h
@@ -7,48 +7,48 @@
 #if		defined	S32_64
 	#define N 16
 	#define M 4
-	#define R 32
+	#define T 32
 #elif	defined S48_72
 	#define N 24
 	#define M 3
-	#define R 36
+	#define T 36
 #elif	defined S48_96
 	#define N 24
 	#define M 4
-	#define R 36
+	#define T 36
 #elif	defined S64_96
 	#define N 32
 	#define M 3
-	#define R 42
+	#define T 42
 #elif	defined S64_128
 	#define N 32
 	#define M 4
-	#define R 44
+	#define T 44
 #elif	defined S96_96
 	#define N 48
 	#define M 2
-	#define R 52
+	#define T 52
 #elif	defined S96_144
 	#define N 48
 	#define M 3
-	#define R 54
+	#define T 54
 #elif	defined S128_128
 	#define N 64
 	#define M 2
-	#define R 68
+	#define T 68
 #elif	defined S128_192
 	#define N 64
 	#define M 3
-	#define R 69
+	#define T 69
 #elif	defined S128_256
 	#define N 64
 	#define M 4
-	#define R 72
+	#define T 72
 #else
-	#define N 8
-	#define M 1
-	#define R 0
-	#error "INVALID MODE"
+	#warning "INVALID MODE : S32_64"
+	#define N 16
+	#define M 4
+	#define T 32
 #endif
 
 typedef uint32_t	uint24_t;
@@ -60,9 +60,18 @@ UINT(N, word);
 typedef word 		block[2];
 typedef word 		key[M];
 
-#define	PRINTHEX(x)	printf("%0*x\t", N/4, a)
+#define	PRINTHEX(x)	printf(	"%0*X", N/4, x	)
+#define NEWLINE()	printf(	"\n\r"			)
+#define TAB()		printf(	"\t"			)
 
-void	ROTL(word *x, uint8_t n);
-void	ROTR(word *x, uint8_t n);
+#define	C			(1<<N)-4
+
+word	ROTL		(word x,	uint8_t n	);
+word	ROTR		(word x,	uint8_t n	);
+
+word	F			(word x					);
+void	ROUND		(block *b,	word k		);
+void	KEXP		(key *k					);
+word	ENCRYPT		(block p,	key k		);
 
 #endif
