70419b65d873c7727e554e61db3a08464dec6bdb
ljs2g15ecs <ljs2g15@soton.ac.uk>
2017-11-14 19:30:21 +0000

    Test Vectors
    Added test vectors for all versions

--- Development/SIMON/source/main.c
+++ Development/SIMON/source/main.c
@@ -4,54 +4,61 @@
 
 int		main()
 {
-	printf("Hello There!!!\n\r");
-	
-	printf("|\tN:\t%d\t|\tM:\t%d\t|\tT:\t%d\t|\n", N, M, T);
-	printf("|\tW:\t%d\t|\tB:\t%d\t|\tK:\t%d\t|\n", sizeof(word)*8, sizeof(block)*8, sizeof(key)*8);
-
-	word a;
-	a.v = 0xA<<(N-4);
-	
-						PRINTHEX(a);	TAB();
-	a.v = ROTL(a, 2);	PRINTHEX(a);	TAB();
-	a.v = ROTR(a, 1);	PRINTHEX(a);	TAB();
-	a.v = ROTR(a, 1);	PRINTHEX(a);	TAB();
-	
-	NEWLINE();
-	word b, c;
-	b.v = 12, c.v = (1<<N)-4;
-	PRINTHEX(a);	TAB();	PRINTHEX(b);	TAB();	PRINTHEX(c);	TAB();
-	c.v ^= a.v ^ b.v;		NEWLINE();
-	PRINTHEX(a);	TAB();	PRINTHEX(b);	TAB();	PRINTHEX(c);	TAB();
-	
-	NEWLINE();		NEWLINE();
-	
-	a.v = 0xA3C2;
-	b = a;
-							PRINTHEX(a);	NEWLINE();
-	a.v = ROTL(a, 1);		PRINTHEX(a);	NEWLINE();				a.v = ROTR(a, 1);
-	a.v = ROTL(a, 8);		PRINTHEX(a);	NEWLINE();				a.v = ROTR(a, 8);
-	a.v = ROTL(a, 2);		PRINTHEX(a);	NEWLINE();				a.v = ROTR(a, 2);
-	
-	c.v = F(b);
-							PRINTHEX(c);	NEWLINE();
-	
-	NEWLINE();		NEWLINE();
-
 #if	defined S32_64	
-	key k =		{ 0x1918, 0x1110, 0x0908, 0x0100	};
-	block p =	{ 0x6565, 0x6877					};
-#endif
-#if	defined S48_72	
-	key k =		{ 0x121110, 0x0A0908, 0x020100		};
-	block p =	{ 0x612067, 0x6E696C				};
+	key 	k	=	{	0x1918, 0x1110,
+						0x0908, 0x0100							};
+	block 	p	=	{	0x6565, 0x6877							};
+	block 	c	=	{	0xC69B, 0xE9BB							};
+#elif	defined S48_72	
+	key 	k	=	{	0x121110, 0x0A0908,
+						0x020100								};
+	block 	p 	=	{	0x612067, 0x6E696C						};
+	block 	c	=	{	0xDAE5AC, 0x292CAC						};
+#elif	defined	S48_96
+	key 	k	=	{	0x1A1919, 0x121110,
+						0x0A0908, 0x020100						};
+	block	p	=	{	0x726963, 0x20646E						};
+	block	c	=	{	0x6E06A5, 0xACF156						};
+#elif	defined S64_96
+	key 	k	=	{	0x13121110, 0x0B0A0908,
+						0x03020100								};
+	block	p	=	{	0x6F722067, 0x6E686C63					};
+	block	c	=	{	0x5CA2E27F, 0x111A8FC8					};
+#elif	defined S64_128
+	key 	k	=	{	0x1B1A1918, 0x13121110,
+						0x0B0A0908, 0x03020100					};
+	block	p	=	{	0x656B696C, 0x20646E75					};
+	block	c	=	{	0x44C8FC20, 0xB9DFA07A					};
+#elif	defined S96_96
+	key 	k	=	{	0x0D0C0B0A0908, 0x050403020100			};
+	block	p	=	{	0x2072616C6C69, 0x702065687420			};
+	block	c	=	{	0x602807A462b4, 0x69063D8FF082			};
+#elif	defined S96_144
+	key 	k	=	{	0x151413121110, 0x0D0C0B0A0908,
+						0x050403020100							};
+	block	p	=	{	0x746168742074, 0x73756420666f			};
+	block	c	=	{	0xECAD1C6C451E, 0x3f59C5DB1AE9			};
+#elif	defined S128_128
+	key 	k	=	{	0x0F0E0D0C0B0A0908, 0x0706050403020100	};
+	block	p	=	{	0x6373656420737265, 0x6C6C657661727420	};
+	block	c	=	{	0x49681B1E1E54FE3F, 0x65AA832AF84E0BBC	};
+#elif	defined S128_192
+	key 	k	=	{	0x1716151413121110, 0x0F0E0D0C0B0A0908,
+						0x0706050403020100						};
+	block	p	=	{	0x206572656874206E, 0x6568772065626972	};
+	block	c	=	{	0xC4AC61EFFCDC0D4F, 0x6C9C8D6E2597b85B	};
+#elif	defined S128_256
+	key 	k	=	{	0x1F1E1D1C1B1A1918, 0x1716151413121110,
+						0x0F0E0D0C0B0A0908, 0x0706050403020100	};
+	block	p	=	{	0x74206E69206D6F6F, 0x6D69732061207369	};
+	block	c	=	{	0x8D2B5579AFC8A3A0, 0x 3BF72A87EfE7b868	};
 #endif
 	
 	TYPE(8) i;
-	for(i=0; i<M; i++)		PRINTHEX(k[i].v);	NEWLINE();
-	PRINTHEX(p[0].v);		PRINTHEX(p[1].v);	NEWLINE();
+	for(i=0; i<M; i++)		PRINTHEX(k[i].v);		NEWLINE();
+	PRINTHEX(p[0].v);		PRINTHEX(p[1].v);		NEWLINE();
 	
-	block cipher;
+	block cipher = {0,0};
 	PRINTHEX(cipher[0].v);	PRINTHEX(cipher[1].v);	NEWLINE();
 	
 	ENCRYPT(p, cipher, k);
