38870e693324afadf85f8f18ee62e8e5000b624c
ljs2g15ecs <ljs2g15@soton.ac.uk>
2017-11-16 12:49:02 +0000

    Update
    In loop key expansion working
    Initial start on progress report

--- Development/SIMON/source/SIMON_host.c
+++ Development/SIMON/source/SIMON_host.c
@@ -21,13 +21,15 @@ TYPE(N)	ROTR	(word x,	uint8_t n	)
 
 TYPE(N)	F		(word x					)
 {
-	/*PRINTHEX(x.v);			NEWLINE();	NEWLINE();
+	/*
+	PRINTHEX(x.v);			NEWLINE();	NEWLINE();
 	PRINTHEX(ROTL(x,1));	NEWLINE();
 	PRINTHEX(ROTL(x,8));	NEWLINE();
 	PRINTHEX(ROTL(x,1) & ROTL(x,8));	NEWLINE();	NEWLINE();
 	PRINTHEX(ROTL(x,1) & ROTL(x,8));	NEWLINE();
 	PRINTHEX(ROTL(x,2));	NEWLINE();
-	PRINTHEX((ROTL(x,1) & ROTL(x,8)) ^ ROTL(x, 2));	NEWLINE();*/
+	PRINTHEX((ROTL(x,1) & ROTL(x,8)) ^ ROTL(x, 2));	NEWLINE();
+	*/
 	return (ROTL(x,1) & ROTL(x,8)) ^ ROTL(x, 2);
 }
 
@@ -68,9 +70,17 @@ void	KEXP	(keys ks,	key k		)
 #endif
 		tmp.v ^= ROTR(tmp, 1);
 		ks[i].v = ~ks[i-M].v ^ tmp.v ^ (z[j][(i-M) % 62]) ^ 3;
+		
+		/*
+		PRINTHEX(ks[i-3].v);	TAB();	PRINTHEX(ks[i-2].v);	TAB();
+		PRINTHEX(ks[i-1].v);	TAB();	PRINTHEX(ks[i].v);		TAB();
+		PRINTHEX(ks[i-M].v);
+		NEWLINE();
+		*/
 	}
 	
-	/*for(i=0; i<T; i++)
+	/*
+	for(i=0; i<T; i++)
 	{
 		PRINTHEX(ks[i].v);	NEWLINE();
 	}*/
@@ -80,21 +90,52 @@ void	ENCRYPT	(const block p,	block c,	key k		)
 {
 	c[0].v = p[0].v;
 	c[1].v = p[1].v;
+	key ks;
 	//keys ks;
 	//KEXP(ks, k);
 	
 	word tmp;
-	TYPE(8) i;
+	TYPE(8) i, l;
+	for(i=M; i>0; i--)	ks[i-1].v = k[M-i].v;
+	
 	for(i=0; i<T; i++)
 	{
 		/*
-		PRINTHEX(c[0].v);	TAB();	PRINTHEX(c[1].v);	TAB();
-		PRINTHEX(ks[i].v);	NEWLINE();
-		*/
-		ROUND(c, ks[i]);
+		PRINTHEX(c[0].v);			PRINTHEX(c[1].v);	TAB();
+		for(l=0; l<M; l++)	{		PRINTHEX(ks[l]);	TAB();	}
+		NEWLINE();		
+		//*/
+		ROUND(c, ks[0]);
 		/*
-		PRINTHEX(c[0].v);	TAB();	PRINTHEX(c[1].v);	TAB();
-		PRINTHEX(ks[i].v);	NEWLINE();					NEWLINE();
-		*/
+		PRINTHEX(c[0].v);			PRINTHEX(c[1].v);	TAB();
+		for(l=0; l<M; l++)	{		PRINTHEX(ks[l]);	TAB();	}
+		NEWLINE();	NEWLINE();
+		//*/
+		
+		///*
+		//	Testing Key Expansion in Loop
+		tmp.v = ROTR(ks[M-1], 3);
+#if (M==4)
+		tmp.v ^= ks[M-3].v;
+#endif
+		tmp.v ^= ROTR(tmp, 1);
+		tmp.v = ~ks[0].v ^ tmp.v ^ (z[j][i % 62]) ^ 3;
+		
+		/*
+		PRINTHEX(c[0].v);			PRINTHEX(c[1].v);	TAB();
+		//for(l=0; l<M; l++)	{		PRINTHEX(ks[l]);	TAB();	}
+		PRINTHEX(tmp.v);	TAB();
+		PRINTHEX(ks[3]);	TAB();	PRINTHEX(ks[2]);	TAB();
+		PRINTHEX(ks[1]);	TAB();	PRINTHEX(ks[0]);	TAB();
+		NEWLINE();		
+		//*/
+		ks[0] = ks[1];
+#if (M>2)
+		ks[1] = ks[2];
+#endif
+#if (M>3)
+		ks[2] = ks[3];
+#endif
+		ks[M-1] = tmp;		
 	}
 }
